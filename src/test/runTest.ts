6cef8d4d4fed540f909af28e096b2b6256cdf4d4
LuisReinoso <lomejordejr@gmail.com>
2023-01-21 04:31:07 -0500

    feat(style-config): add support for newProjectRoot attribute to load style config
    Issue #8

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,21 +1,28 @@
-import * as path from "path";
+import * as path from 'path';
 
-import { runTests } from "vscode-test";
+import { runTests } from 'vscode-test';
 
 async function main() {
   try {
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
-    const extensionDevelopmentPath = path.resolve(__dirname, "../../");
+    const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
-		const testWorkspace = path.resolve(__dirname, "../../workspaces/workspace");
-    const testWorkspaceWithDefaultProject = path.resolve(__dirname, "../../workspaces/workspace-default-project/");
+    const testWorkspace = path.resolve(__dirname, '../../workspaces/workspace');
+    const testWorkspaceWithDefaultProject = path.resolve(
+      __dirname,
+      '../../workspaces/workspace-default-project/'
+    );
+    const testWorkspaceNewProjectRoot = path.resolve(
+      __dirname,
+      '../../workspaces/workspace-new-project-root/'
+    );
 
-		console.log('workspace should be', testWorkspace);
+    console.log('workspace should be', testWorkspace);
 
     // The path to test runner
     // Passed to --extensionTestsPath
-    const extensionTestsPath = path.resolve(__dirname, "./suite/index");
+    const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
     // Download VS Code, unzip it and run the integration test
     await runTests({
@@ -24,7 +31,7 @@ async function main() {
       launchArgs: [
         testWorkspace,
         // This disables all extensions except the one being testing
-        "--disable-extensions",
+        '--disable-extensions',
       ],
     });
 
@@ -34,11 +41,31 @@ async function main() {
       launchArgs: [
         testWorkspaceWithDefaultProject,
         // This disables all extensions except the one being testing
-        "--disable-extensions",
+        '--disable-extensions',
+      ],
+    });
+
+    await runTests({
+      extensionDevelopmentPath,
+      extensionTestsPath,
+      launchArgs: [
+        testWorkspaceWithDefaultProject,
+        // This disables all extensions except the one being testing
+        '--disable-extensions',
+      ],
+    });
+
+    await runTests({
+      extensionDevelopmentPath,
+      extensionTestsPath,
+      launchArgs: [
+        testWorkspaceNewProjectRoot,
+        // This disables all extensions except the one being testing
+        '--disable-extensions',
       ],
     });
   } catch (err) {
-    console.error("Failed to run tests");
+    console.error('Failed to run tests');
     process.exit(1);
   }
 }
